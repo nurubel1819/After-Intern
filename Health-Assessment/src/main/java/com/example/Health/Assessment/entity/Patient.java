b8f8ad0f8bc9508540b9525155b12cb9eb70f6c2
nurubel1819 <rubel.18cse255@bsmrstu.edu.bd>
2025-07-12 18:35:04 +0600

    SOme update

--- Health-Assessment/src/main/java/com/example/Health/Assessment/entity/Patient.java
+++ Health-Assessment/src/main/java/com/example/Health/Assessment/entity/Patient.java
@@ -22,8 +22,16 @@ public class Patient {
     private String patientName;
     private int patientAge;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    /*@OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "patient_id")
+    private List<Disease> diseases = new ArrayList<>();*/
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(
+            name = "patient_disease",
+            joinColumns = @JoinColumn(name = "patient_id"),
+            inverseJoinColumns = @JoinColumn(name = "disease_id")
+    )
     private List<Disease> diseases = new ArrayList<>();
 
     @OneToMany(cascade = CascadeType.ALL)
